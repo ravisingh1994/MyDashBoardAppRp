1f41873c2c82416b01fec3037738bd933efd44f3
kushwahaprakash <prakashkm046@gmail.com>
2022-03-22 02:44:28 +0530

    Add project files.

---
+++ MyDashBoardAppRp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using MyDashBoardAppRp.DB_Emp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyDashBoardAppRp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult IndexDashBoard()
+        {
+            return View();
+        }
+
+        public ActionResult EmpTable()
+        {
+            EmployeeDataEntities ent = new EmployeeDataEntities();
+
+            var data=ent.EmployeeDetails.ToList();
+
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MyDashBoardAppRp/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyDashBoardAppRp.Models
+{
+    public class EmployeeModel
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        public string City { get; set; }
+
+    }
+}
